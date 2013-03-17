bee610c79715714e9dd36a65c37ccce2d3aad270
Alvin <alvin.c.gonzales@gmail.com>
2013-03-17 22:55:21 +0800

    New computation for az/alt, seems to match stelarium results perfectly

--- src/upstars_lib/projectors.py
+++ src/upstars_lib/projectors.py
@@ -41,9 +41,10 @@ class AzAltProjector:
         ra_h, dec = object_radec
         ra = ra_h / 24 * 360
         lon, lat = self.reference_lonlat
-        ha = self.last - ra
-
-        az, alt = self.compute_azalt(dec, lat, ha)
+#        ha = self.last - ra
+        az, alt = coord_to_horizon(self.date, ra, dec, lat, lon)
+        az = az / 360 * 24
+ #       az, alt = self.compute_azalt(dec, lat, ha)
 
         return AzAlt(az, alt)
 
@@ -88,6 +89,81 @@ def _utc_to_jd(utc):
 
 
 #assert _utc_to_jd(datetime(2000, 1, 1, 12)) ==  2451545.0, '_utc_to_jd'
+
+
+# http://www.convertalot.com/celestial_horizon_co-ordinates_calculator.html
+#// compute horizon coordinates from ra, dec, lat, lon, and utc
+#// ra, dec, lat, lon in degrees
+#// utc is a Date object
+#// results returned in hrz_altitude, hrz_azimuth
+def coord_to_horizon( utc, ra, dec, lat, lon ):
+    ha = mean_sidereal_time( utc, lon ) - ra;
+    if (ha < 0):
+         ha = ha + 360
+
+    # convert degrees to radians
+    ha = ha*pi/180
+    dec = dec*pi/180
+    lat = lat*pi/180
+
+    # compute altitude in radians
+    sin_alt = sin(dec)*sin(lat) + cos(dec)*cos(lat)*cos(ha);
+    alt = asin(sin_alt)
+
+    # compute azimuth in radians
+    # divide by zero error at poles or if alt = 90 deg
+    cos_az = (sin(dec) - sin(alt)*sin(lat))/(cos(alt)*cos(lat))
+    az = acos(cos_az)
+
+    # convert radians to degrees
+    hrz_altitude = alt*180/pi;
+    hrz_azimuth = az*180/pi;
+
+    # choose hemisphere
+    if (sin(ha) > 0):
+        hrz_azimuth = 360 - hrz_azimuth
+
+    print (utc, ra, dec, lat, lon, hrz_azimuth, hrz_altitude)
+    return hrz_azimuth, hrz_altitude
+
+
+#// Compute the Mean Sidereal Time in units of degrees.
+#// Use lon := 0 to get the Greenwich MST.
+#// East longitudes are positive; West longitudes are negative
+#// returns: time in degrees
+def mean_sidereal_time(now, lon):
+    year = now.year
+    month = now.month
+    day = now.day
+    hour = now.hour
+    minute = now.minute
+    second = now.second
+
+    print year, month, day, hour, minute, second
+
+    if ((month == 1) or (month == 2)):
+        year = year - 1
+        month = month + 12
+
+    a = floor(year/100.0);
+    b = 2 - a + floor(a/4.0);
+    c = floor(365.25*year);
+    d = floor(30.6001*(month + 1));
+
+    # days since J2000.0
+    jd = b + c + d - 730550.5 + day + (hour + minute/60.0 + second/3600.0)/24.0;
+
+    # julian centuries since J2000.0
+    jt = jd/36525.0
+
+    # the mean sidereal time in degrees
+    mst = 280.46061837 + 360.98564736629*jd + 0.000387933*jt*jt - jt*jt*jt/38710000 + lon;
+    mst = mst % 360.0
+    print "abcd", a, b, c, d
+    print "jd/jt", jd, jt
+    print "mst:", mst
+    return mst
+
 
 class TileProjector():
     def __init__(self, tile_size, bounds):
