a7d09a238401cf22268dc6745d8744e18e1124bb
Alvin <alvin.c.gonzales@gmail.com>
2013-03-21 21:54:58 +0800

    Only register debug routes when we are running on dev server

--- src/upstars.py
+++ src/upstars.py
@@ -7,16 +7,16 @@ app = Flask(__name__, static_folder='rootstatic')
 app.register_blueprint(upstars_home.blueprint)
 app.register_blueprint(upstars_svgtiles.blueprint, url_prefix='/tiles')
 
-@app.route("/debug/routes")
-def debug_routes():
-    links = []
-    for rule in app.url_map.iter_rules():
-        links.append(repr(rule))
-
-    routes = "\n".join(links)
-    return Response(response=routes,
-                    status=200,
-                    mimetype="text/plain")
-
 if __name__ == "__main__":
+    @app.route("/debug/routes")
+    def debug_routes():
+        links = []
+        for rule in app.url_map.iter_rules():
+            links.append(repr(rule))
+
+        routes = "\n".join(links)
+        return Response(response=routes,
+                        status=200,
+                        mimetype="text/plain")
+
     app.run(host="0.0.0.0", debug=True, threaded=True)
